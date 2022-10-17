46702cfb7dac82c059148d16392c40ad02ee9c5b
TessyChuks <perpetuachukwuma10@gmail.com>
2022-10-17 10:39:59 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,16 +12,16 @@ int main(void)
 	{
 		for (j = i + 1; j <= 99; j++)
 		{
-			pputchar('0' + i / 10);
+			putchar('0' + i / 10);
 			putchar('0' + i % 10);
 
 			putchar(' ');
 
 			putchar('0' + j / 10);
-			putchar('0' + i j % 10);
+			putchar('0' + j % 10);
 
 			if (i == 98 && j == 99)
-				break
+				break;
 			putchar(',');
 			putchar(' ');
 		}
