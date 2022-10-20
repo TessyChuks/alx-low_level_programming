ed0686339eff07f0355eafb9592ae17f04abf491
TessyChuks <perpetuachukwuma10@gmail.com>
2022-10-20 14:19:02 -0700

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,33 +1,12 @@
 #include "main.h"
 
 /**
- * print_triangle - entry point
- * Description: print diagonals
- * @size: size of the triangle
- * Return: void
+ * mul - multipliesvtwo integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplication of a and b
  */
-
-void print_triangle(int size)
+int mul(int a, int b)
 {
-	int row, hashes, spaces;
-
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (row = 1; row <= size; row++)
-		{
-			for (spaces = size - row; spaces >= 1; spaces--)
-			{
-				_putchar(' ');
-			}
-			for (hashes = 1; hashes <= row; hashes++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+	return (a * b);
 }
